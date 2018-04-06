a4793baab5ad56494fa567380cdd28c4ffc00577
[Martina] <[tinag55@hotmail.com]>
2018-04-06 11:26:46 -0300

    Abril

---
+++ Zoo/js/koala.js
@@ -0,0 +1,30 @@
+animal=0;
+patas= 0;
+comida=0;
+kilos=0;
+elefante=true;
+babar=true;
+
+if (patas=2 && comida=herviboro){
+  kilos="1.5kg";
+  console.log("Alimento":+kilos +comida)
+}
+
+if (patas=4 && comida=herviboro){
+  kilos="3kg";
+  console.log("Alimento":+kilos +comida)
+}
+
+if (patas=4 && comida=carnivoro){
+  kilos="5kg";
+  console.log("Alimento":+kilos +comida)
+}
+
+if (patas=0){
+  kilos="cobayo";
+  console.log("Alimento":+kilos)
+}
+if (elefante || babar) {
+  kilos="20kg"
+  console.log("Alimento":+kilos)
+}
